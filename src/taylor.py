5f5894e747fe9c66e67df0bcfb26b5b991d5a13b
HERATH MUDIYANSELAGE Nuwan <nuwan.herath-mudiyanselage@inria.fr>
2021-03-26 15:41:18 +0100

    Bug

--- src/taylor.py
+++ src/taylor.py
@@ -1,7 +1,7 @@
 import argparse
 import os
 import numpy as np
-from math import cos, pi, factorial
+from math import cos, pi, factorial, isclose
 import scipy.fftpack as fp
 from utils_taylor import vanishes
 import flint as ft
@@ -42,15 +42,16 @@ grid = [cos((2 * i + 1) * pi / (2 *n)) for i in range(0, n)]
 def corrected_idct(poly, n):
     return np.array([(x + poly[0]) / 2 for x in fp.idct(poly, n=n)])
 
-p = np.empty((n,deg_x+1))
-for j in range(deg_x+1):
-    _p = np.polynomial.chebyshev.poly2cheb(poly[j])
-    p[:,j] = corrected_idct(_p, n)
+p = np.empty((n,deg_y+1))
+for d in range(deg_y+1):
+    _p = np.polynomial.chebyshev.poly2cheb(poly[:, d])
+    p[:, d] = corrected_idct(_p, n)
 
-# for j in range(deg_x+1):
-#     _p = np.polynomial.Polynomial(poly[j])
-#     print(_p)
-#     p[:,j] = [_p(e) for e in grid]
+
+# q = np.empty((n,n))
+# for i in range(n):
+#     for j in range(n):
+#         q[i,j] = np.polynomial.polynomial.polyval2d(grid[i], grid[j], poly)
 
 d = len(p[0]) - 1
 p_der = np.zeros((m,n))
@@ -63,7 +64,10 @@ for i in range(n):
         tmp = np.polynomial.chebyshev.chebder(_p, k)
         p_der[k,:] = 1/factorial(k) * corrected_idct(tmp, n)
     for j in range(n):
-        if vanishes(ft.arb_poly(p_der[:,j].tolist()), a, ft.arb(grid[j]), grid, j, m, d):
+        # if not isclose(p_der[0, j], q[i,j]):
+        #         print(f"{p_der[0, j]} {q[i, j]}")
+        #         print("oups")
+        if vanishes(ft.arb_poly(p_der[:,j].tolist()), a, grid, j, m, d):
             l.append((i,j))
 
 print(l)
--- src/utils_taylor.py
+++ src/utils_taylor.py
@@ -1,28 +1,48 @@
 import flint as ft
-from math import floor
+from math import floor, pi, cos, factorial
 from scipy.special import comb
 
-def bound(interval, a, points, i, m, d):
+def bound(a, points, i, m, d):
     r_left = ft.arb(points[i] - points[i-1]) / 2 if 0 < i else ft.arb(0)
     r_right = ft.arb(points[i+1] - points[i]) / 2 if i < len(points) - 1 else ft.arb(0)
     r = r_left.max(r_right)
-    return a * r**(m+1) * ft.arb.max(1/(1 - interval.abs_upper() -r)**(m+2), comb(d+1, m+2))
+    return a * r**(m+1) * ft.arb.max(1/(1 - ft.arb(points[i]).abs_upper() -r)**(m+2), comb(d+1, m+2))
 
-def vanishes(poly, a, interval, points, i, m, d):
-    b = bound(interval, a, points, i, m, d)
-    ball = poly(interval) + ft.arb(0, b.abs_upper())
+def vanishes(poly, a, points, i, m, d):
+    b = bound(a, points, i, m, d)
+    # print(poly(ft.arb(points[i])))
+    # print(b)    
+    ball = poly(ft.arb(points[i])) + ft.arb(0, b.abs_upper())
     # print(ball)
     return 0 in ball
 
 if __name__ == "__main__":
-    from math import cos, pi
     import numpy as np
-    n = 128
-    i = floor(n/2)
+    import scipy.fftpack as fp
+
+    def corrected_idct(poly, n):
+        return np.array([(x + poly[0]) / 2 for x in fp.idct(poly, n=n)])
+
+    p_can = [0, 0, 1]
+    p_cheb = np.polynomial.chebyshev.poly2cheb(p_can)
+    n = 20
+    m = 1
+    p_der = np.zeros((m+1,n))
+    for k in range(m+1):
+        tmp = np.polynomial.chebyshev.chebder(p_cheb, k)
+        p_der[k,:] = 1/factorial(k) * corrected_idct(tmp, n)
     grid = [cos((2 * i + 1) * pi / (2 *n)) for i in range(0, n)]
-    a = ft.arb((2*i+1)/(2*n)).cos_pi()
-    b = ft.arb((2*(i-1)+1)/(2*n)).cos_pi()
-    print((a - b).abs_upper())
-    print((a - b).abs_lower())
-    diff = np.abs(np.diff(grid))
-    print(np.max(diff))
+    for i in range(n):
+        print(vanishes(ft.arb_poly(p_der[:,i].tolist()), 1, grid, i, m, 2))
+
+    print(p_cheb.shape)
+    print(p_der[:,10].shape)
+
+    print(grid[10])
+    # print(grid[9])
+    r = max(grid[11] - grid[10], grid[10] - grid[9]) / 2
+    ball = ft.arb_poly(p_der[:,10].tolist())(ft.arb(grid[10],r))
+    print(ball)
+    print(0 in ball)
+    # print(vanishes(ft.arb_poly([0,1]), 1,[0,1,2,3],2,2,1))
+    # print(vanishes(ft.arb_poly([0,1]), 1,[0,1,2,3],0,2,1))
