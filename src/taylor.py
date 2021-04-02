42adb9e6ede85454c7f52915a8315ffdcf0f08c2
HERATH MUDIYANSELAGE Nuwan <nuwan.herath-mudiyanselage@inria.fr>
2021-04-02 10:50:16 +0200

    Added timing

--- src/taylor.py
+++ src/taylor.py
@@ -1,12 +1,17 @@
 import argparse
 import os
+
 import numpy as np
 from math import cos, pi, factorial, isclose
 import scipy.fftpack as fp
 from utils_taylor import vanishes
 import flint as ft
+from scipy.special import comb
+
 import matplotlib.pyplot as plt
 from matplotlib import collections as mc
+
+from codetiming import Timer
 
 # Parse the input
 
@@ -18,6 +23,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('n', type=int, help="size of the grid (number of subdivision intervals)")
 parser.add_argument('-poly', type=str, default=default_file, help="file of polynomial coefficients")
 parser.add_argument('-m', type=int, default=3, help="precision of the approximation")
+# parser.add_argument('-elliptic', help="use elliptic coefficients for the polynomial", action="store_true")
 
 args = parser.parse_args()
 
@@ -39,31 +45,56 @@ poly = np.loadtxt(args.poly, dtype=int)
 
 # Core of the program
 
+# if args.elliptic:
+#     poly = np.multiply(poly, comb2D(deg_x + 1, deg_y + 1))
+
 grid = [cos((2 * i + 1) * pi / (2 *n)) for i in range(0, n)]
 
 def corrected_idct(poly, n):
     return np.array([(x + poly[0]) / 2 for x in fp.idct(poly, n=n)])
 
+
 p = np.empty((n,deg_y+1))
-for d in range(deg_y+1):
-    _p = np.polynomial.chebyshev.poly2cheb(poly[:, d])
-    p[:, d] = corrected_idct(_p, n)
+with Timer("conversion", logger=None):
+    for d in range(deg_y+1):
+        _p = np.polynomial.chebyshev.poly2cheb(poly[:, d])
+        p[:, d] = corrected_idct(_p, n)
 
 d = len(p[0]) - 1
 p_der = np.zeros((m,n))
 
+hockeystick = ft.arb(comb(d+1, m+2))
+radii = []
+with Timer("radii", logger=None):
+    for i in range(n):
+        r_left = -ft.arb(grid[i] - grid[i-1]) / 2 if 0 < i else ft.arb(0)
+        r_right = -ft.arb(grid[i+1] - grid[i]) / 2 if i < n - 1 else ft.arb(0)
+        radii.append(ft.arb.max(r_left, r_right))
 intervals = np.empty(n, dtype="object")
 for i in range(n):
     indices = []
-    _p = np.polynomial.chebyshev.poly2cheb(p[i])
+    with Timer("conversion 2", logger=None):
+        _p = np.polynomial.chebyshev.poly2cheb(p[i])
     a = max(p[i], key=abs)
-    for k in range(m):
-        tmp = np.polynomial.chebyshev.chebder(_p, k)
-        p_der[k,:] = 1/factorial(k) * corrected_idct(tmp, n)
+    with Timer("approximation", logger=None):
+        for k in range(m):
+            tmp = np.polynomial.chebyshev.chebder(_p, k)
+            p_der[k,:] = 1/factorial(k) * corrected_idct(tmp, n)
     for j in range(n):
-        if vanishes(ft.arb_poly(p_der[:,j].tolist()), a, grid, j, m, d):
-            indices.append((i,j))
+        with Timer("isolation", logger=None):
+            if vanishes(ft.arb_poly(p_der[:,j].tolist()), a, grid[j], m, hockeystick, radii[i]):
+                indices.append((i,j))
     intervals[i] = indices
+timers = Timer.timers
+print(f"""radii: {timers['radii']}
+conversion (x): {timers['conversion']}
+conversion (y): {timers['conversion 2']}
+approximation: {timers['approximation']}
+isolation: {timers['isolation']}
+    bound: {timers['bound']}
+        ogf: {timers['ogf']}
+        res: {timers['res']}
+    eval: {timers['eval']}""")
 
 # Show isolated intervals
 
--- src/utils_taylor.py
+++ src/utils_taylor.py
@@ -1,21 +1,22 @@
 import flint as ft
-from math import floor, pi, cos, factorial
-from numpy.core.defchararray import rpartition
-from scipy.special import comb
+from math import pi
 
-def bound(a, points, i, m, d, r):
-    # bound with the hockey-stick identity
-    hs = ft.arb(comb(d+1, m+2))
+
+from codetiming import Timer
+
+def bound(a, c, m, r, hs):
     # bound with an ordinary generating function
-    ogf = 1/(1 - ft.arb(points[i]).abs_upper() -r)**(m+2)
+    with Timer("ogf", logger=None):
+        ogf = 1/(1 - ft.arb(c).abs_upper() -r)**(m+2)
     if (ogf.lower() < 0): # force hs if ogf has negative values
         ogf = hs + 1
-    return a * r**(m+1) * ft.arb.min(ogf, hs)
+    with Timer("res", logger=None):
+        res = a * r**(m+1) * ft.arb.min(ogf, hs)
+    return res
 
-def vanishes(poly, a, points, i, m, d):
-    r_left = -ft.arb(points[i] - points[i-1]) / 2 if 0 < i else ft.arb(0)
-    r_right = -ft.arb(points[i+1] - points[i]) / 2 if i < len(points) - 1 else ft.arb(0)
-    r = r_left.max(r_right)
-    b = bound(a, points, i, m, d, r)
-    ball = poly(ft.arb(0,r)) + ft.arb(0, b.abs_upper())
+def vanishes(poly, a, c, m, hs, r):
+    with Timer("bound", logger=None):
+        b = bound(a, c, m, r, hs)
+    with Timer("eval", logger=None):
+        ball = poly(ft.arb(0,r.abs_upper())) + ft.arb(0, b.abs_upper())
     return 0 in ball
